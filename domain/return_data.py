bb0c343ddb4cc5da513f73c8e4bc22b4a2229892
gasparnovel <gnovel@cifpfbmoll.eu>
2021-03-29 08:10:15 +0200

    feat: :sparkles: data.py
    Añadimos el archivo para almacenar los datos y el schema dados por JOOPbox

--- domain/return_data.py
+++ domain/return_data.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource
+from resources.data import Data
 import json
 
 # devuelve datos
 class Return_data(Resource):
     def get(self):
-        return {'Prueba': 'Inicial'}
+        return Data.datos, 200
